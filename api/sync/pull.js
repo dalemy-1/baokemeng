c0b5b4c3b0d59b2318c402c5ee13b4055e008b51
dalemy-1 <9848014@gmail.com>
2026-01-07 03:21:18 +0800

    Update pull.js

--- api/sync/pull.js
+++ api/sync/pull.js
@@ -1,3 +1,15 @@
+function withCors(res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-admin-token');
+}
+
+export default async function handler(req, res) {
+  withCors(res);
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
+  // ...你原来的逻辑
+}
 import { cors, handleOptions, auth, getSupabase, serverErr } from "./_util.js";
 
 export default async function handler(req, res) {
