1d445d89b66be09902883ec8971ea9b6915855d1
dalemy-1 <9848014@gmail.com>
2026-01-07 03:23:26 +0800

    Update _util.js

--- api/sync/_util.js
+++ api/sync/_util.js
@@ -1,15 +1,16 @@
-function withCors(res) {
+export function withCors(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-admin-token');
+
+  // 预检请求直接返回
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return true;
+  }
+  return false;
 }
 
-export default async function handler(req, res) {
-  withCors(res);
-  if (req.method === 'OPTIONS') return res.status(200).end();
-
-  // ...你原来的逻辑
-}
 import { createClient } from "@supabase/supabase-js";
 
 export function cors(res) {
